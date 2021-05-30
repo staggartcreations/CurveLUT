2e6393ffcf3321630962d4559e2b354a12f6ac54
Jonathan van Immerzeel <jonathan.immerzeel@gmail.com>
2021-05-30 21:17:04 +0200

    Initial version

---
+++ CurveLUT.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace CurveLUT
+{
+    public class CurveLUT
+    {
+        //Resolutions lower than 256 will start to show a grey line at the bottom
+        //High resolutions aren't needed thanks to bilinear interpolation between texels
+        private const int resolution = 256;
+        
+        public static Texture2D Create(AnimationCurve curveA, AnimationCurve curveB, AnimationCurve curveC, AnimationCurve curveD)
+        {
+            //32-bit precision since curve values can exceed 1
+            Texture2D LUTTex = new Texture2D(resolution, 8, TextureFormat.RGBAFloat, false, true)
+            {
+                name = "LUT",
+                anisoLevel = 0,
+                filterMode = FilterMode.Bilinear,
+                wrapMode = TextureWrapMode.Clamp
+            };
+
+            for (int x = 0; x < resolution; x++)
+            {
+                //0-1 value, distance from left to right
+                float t = (float)x / (float)resolution;
+                
+                Color gradientPixel = new Color(
+                    (curveA != null ? curveA.Evaluate(t) : 0f),
+                    (curveB != null ? curveB.Evaluate(t) : 0f),
+                    (curveC != null ? curveC.Evaluate(t) : 0f),
+                    (curveD != null ? curveD.Evaluate(t) : 0f)
+                    );
+
+                for (int y = 0; y < resolution; y++)
+                {
+                    LUTTex.SetPixel(x, y, gradientPixel);
+                }
+            }
+
+            LUTTex.Apply();
+
+            return LUTTex;
+        }
+    }
+}
---
+++ Editor/CurveLUTImporter.cs
@@ -0,0 +1,74 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+#if UNITY_2020_2_OR_NEWER
+using UnityEditor.AssetImporters;
+#else
+using UnityEditor.Experimental.AssetImporters;
+#endif
+
+namespace CurveLUT
+{
+    [ScriptedImporter(1, "curveLUT")]
+    public class CurveLUTImporter : ScriptedImporter
+    {
+        public AnimationCurve curveA = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+        public AnimationCurve curveB = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+        public AnimationCurve curveC = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+        public AnimationCurve curveD = AnimationCurve.Linear(0f, 0f, 1f, 1f);
+        
+        public override void OnImportAsset(AssetImportContext context)
+        {
+            Texture2D lut = CurveLUT.Create(curveA, curveB, curveC, curveD);
+
+            context.AddObjectToAsset("Look up Texture", lut);
+            context.SetMainObject(lut);
+        }
+        
+        [MenuItem("Assets/Create/Curve LUT")]
+        private static void CreateCurveLUTAsset()
+        {
+            string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+            if (path == "")
+            {
+                path = "Assets";
+            }
+            else if (Path.GetExtension(path) != "")
+            {
+                path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+            }
+
+            string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New Curve LUT.curveLUT");
+
+            File.Create(assetPathAndName);
+
+            AssetDatabase.SaveAssets();
+            AssetDatabase.Refresh();
+
+            Texture2D lut = AssetDatabase.LoadAssetAtPath<Texture2D>(assetPathAndName);
+
+            Selection.activeObject = lut;
+        }
+    }
+    
+    [CustomEditor(typeof(CurveLUTImporter))]
+    public class CubeImporterEditor: ScriptedImporterEditor
+    {
+        private CurveLUTImporter importer;
+
+        public override void OnEnable()
+        {
+            importer = (CurveLUTImporter)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            EditorGUI.BeginChangeCheck();
+            
+            base.OnInspectorGUI();
+            
+            base.ApplyRevertGUI();
+        }
+    }
+}
